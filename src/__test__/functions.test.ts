75a8ce7670e1d19a9bf1b675a87000da9559295d
losalmson <mialo.salmson@gmail.com>
2024-04-16 11:57:36 +0200

    added unit tests for funtcions to mark todos as done and to remove todos

--- src/__test__/functions.test.ts
+++ src/__test__/functions.test.ts
@@ -1,21 +1,46 @@
-import { addTodo } from "../ts/functions";
+import { addTodo, changeTodo, removeAllTodos } from "../ts/functions";
 import { Todo } from "../ts/models/todo";
 
 describe("Testing todos", () => {
     test("it should add todo to list", () => {
     
-        //Assign 
-    const todoText = "Spela Minecraft";
+    //Assign 
+    const todoText: string = "Spela Minecraft";
     const theList: Todo[] = [];
     const lengthTodoText = todoText.length;
 
-//Act 
-addTodo(todoText, theList);
+    //Act 
+    addTodo(todoText, theList);
 
-//Assert
-expect(theList.length).toBeGreaterThan(0);
-expect(lengthTodoText).toBeGreaterThan(2);
+    //Assert
+    expect(theList.length).toBeGreaterThan(0);
+    expect(lengthTodoText).toBeGreaterThan(2);
+    });
+
+test("it should be marked as done correctly", () => {
+    //Assign 
+    const todo: Todo = {text: "Spela Tekken 8", done: false };
+    //Act 
+    changeTodo(todo)
+    //Assert
+    expect(todo.done).toBeTruthy();
+    });
 });
+
+test("it should remove all todos from the list", () => {
+    //Assign 
+    const todosCount = 5;
+    const todos: Todo[] = [];
+
+    for (let i = 1; i <= todosCount; i++) {
+        todos.push({ text: `Todo ${i}`, done: i % 2 === 0 });
+    }
+    
+    //Act
+    removeAllTodos(todos);
+
+    //Assert 
+    expect(todos.length).toBe(0);
 });
 
 
